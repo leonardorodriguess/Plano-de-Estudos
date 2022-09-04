6f6052ca12b3e2fb2f347540043a851a56608b53
Leonardo Rodrigues <leo-feitosa@live.com>
2022-09-04 14:28:33 -0300

    Criado lista

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Botao extends React.Component {
+    render(){
+        return(
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
+
+
+
+export default Botao;
