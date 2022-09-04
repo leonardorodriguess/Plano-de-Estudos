0cd3c27ebf520e7d97bb87944db82816acfad745
Leonardo Rodrigues <leo-feitosa@live.com>
2022-09-04 15:03:34 -0300

    Estilizar com ternário

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 class Botao extends React.Component {
     render(){
+        const backgroundColor = 'blue';
+        const estaAtivo = true
+        
+        const styles = {
+            backgroundColor,
+            color: estaAtivo ? 'white' : 'black',
+        }
+        
         return(
-            <button>
+            <button style={styles}>
                 Botão
             </button>
         )
