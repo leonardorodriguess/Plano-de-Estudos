247966d95cc5f8386d4a7b14e1f9786568344488
Leonardo Rodrigues <leo-feitosa@live.com>
2022-10-09 18:04:25 -0300

    Passando objeto vazio para a lista

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,21 +2,16 @@ import React, { useState } from 'react';
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
+import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss';
 
 function App() {
-  const [tarefas, setTarefas] = useState([{
-      tarefa: 'React',
-      tempo: '02:00:00'
-  },{
-      tarefa: 'JS',
-      tempo: '01:30:00'
-  }]);
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas}/>
-      <Lista tarefas={tarefas}/>
       <Cronometro />
+      <Lista tarefas={tarefas}/>
     </div>
   );
 }
