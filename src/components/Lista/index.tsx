1f11024918ddeb4aec5c6a75249fe24f0b8a67f3
Leonardo Rodrigues <leo-feitosa@live.com>
2022-10-09 16:22:52 -0300

    Adicionando elementos na lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss'
 
 function Lista () {
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00'
     },{
         tarefa: 'JS',
         tempo: '01:30:00'
-    }]
+    }]);
 
     return(
         <aside className={style.ListaTarefas}>
-            <h2 >Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTarefas( [...tarefas, {tarefa : 'cana', tempo : '01:03:00'}])
+            }}>Estudos do dia</h2>
+
             <ul>
                 {tarefas.map((item, index) => (       
                     <Item 
